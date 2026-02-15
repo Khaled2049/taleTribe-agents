959a79547418f677032e82fc13f55ca9cf69ee73
Khaled Hossain <khaledhossain.not@gmail.com>
2026-02-15 13:57:47 -0700

    update yaml

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "gcs" {
+    bucket = "story-6f89f-tfstate"
+    prefix = "novelsync-agents"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
